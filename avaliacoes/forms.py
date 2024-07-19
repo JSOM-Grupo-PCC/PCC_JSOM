e82e4916768773946a54d1d8af47f1c862b1887e
savioomio <saviopessoaafonso@gmail.com>
2024-07-19 06:20:13 -0300

    Adicioando estilização completa do APP de avaliações

--- avaliacoes/forms.py
+++ avaliacoes/forms.py
@@ -6,3 +6,8 @@ class AvaliacaoForm(forms.ModelForm):
     class Meta:
         model = Avaliacao
         fields = ['peso', 'altura', 'observacao']
+        widgets = {
+            'peso': forms.NumberInput(attrs={'class': 'form-control'}),
+            'altura': forms.NumberInput(attrs={'class': 'form-control'}),
+            'observacao': forms.Textarea(attrs={'class': 'form-control'}),
+        }
