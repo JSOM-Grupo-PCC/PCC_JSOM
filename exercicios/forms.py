ae2a4457182ff32f646c2801ea12f59c28029d59
savioomio <saviopessoaafonso@gmail.com>
2024-07-26 17:02:50 -0300

    estilizando formularios de GruposMusculares

--- exercicios/forms.py
+++ exercicios/forms.py
@@ -2,9 +2,11 @@ from django import forms
 from exercicios.models import GrupoMuscular, Exercicio
 
 class GrupoMuscularForm(forms.ModelForm):
+    nome = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control mb-2 text-white'}))
     class Meta:
         model = GrupoMuscular
         fields = ['nome']
+
 
 class ExercicioForm(forms.ModelForm):
     class Meta:
