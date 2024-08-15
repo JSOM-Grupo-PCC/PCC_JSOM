2fd12406846f2ca05a0835fdacbffc7df21c0845
savioomio <saviopessoaafonso@gmail.com>
2024-08-15 02:43:45 -0300

    coletando arquivos staticos e configurando settigs

--- project_jsom/settings.py
+++ project_jsom/settings.py
@@ -26,9 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-b55hj_pe*oh93fn)%hpsh!way!wrhj3y%*m^q&^45%qowshd$q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', ]
+
 
 
 # Application definition
